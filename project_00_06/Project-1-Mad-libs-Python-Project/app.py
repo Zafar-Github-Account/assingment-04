b4c2cab6d9efddda1466893bad2b2d1e0fc43369
zafar <166161455+Zafar-Github-Account@users.noreply.github.com>
2025-04-14 00:46:20 +0500

    Add files via upload

---
+++ project_00_06/Project-1-Mad-libs-Python-Project/app.py
@@ -0,0 +1,15 @@
+# Mad Libs Project
+
+# Getting user inputs
+adj = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+famous_person = input("Enter the name of a famous person: ")
+
+# Creating the mad libs story
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time \
+because I love to {verb1}. Stay hydrated and {verb2} like you’re {famous_person}!"
+
+# Displaying the result
+print("\nHere is your Mad Lib:\n")
+print(madlib)
